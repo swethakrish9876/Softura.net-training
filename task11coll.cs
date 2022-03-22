26123397eccd99dd1ec1582e55d41067db4a3439
swethakrish9876 <101106198+swethakrish9876@users.noreply.github.com>
2022-03-22 16:17:46 +0530

    Add files via upload

---
+++ task11coll.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Collections;
+
+namespace ConsoleApp2
+{
+    class task11coll
+    {
+        public static void Main()
+        {
+
+            FileStream fs = new FileStream("E:\\first\\programming\\coding.txt", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+            StreamWriter bw = new StreamWriter(fs);
+            List<string> str = new List<String>();
+            str.Add("gulab jamun");
+            str.Add("rasamalai");
+            str.Add("choco lava cake");
+            str.Add("rasberry cheesecake");
+            str.Add("bread halwa");
+            foreach (string d in str)
+            {
+                bw.Write("\n" + d);
+            }
+            bw.Flush();
+            bw.Close();
+            fs.Close();
+            FileStream fs1 = new FileStream("E:\\first\\programming\\coding.txt", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+            StreamReader sr = new StreamReader(fs1);
+            String s = sr.ReadToEnd();
+            sr.Close();
+            fs1.Close();
+            Console.WriteLine(s);
+            FileInfo fi = new FileInfo(@"E:\first\programming\trail.txt");
+            Console.WriteLine(fi.Length);
+            Console.WriteLine(fi.CreationTime);
+            Console.Read();
+        }
+    }
+}
+
---
+++ task11employee.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace ConsoleApp2
+{
+    [Serializable]
+    class task11employee
+    {
+        public string id = "k38";
+        public string name = "Krishna";
+        public string gender = "Male";
+        public int age = 21;
+
+
+    }
+    class emp1
+    {
+        
+        public void Seritofile()
+        {
+            task11employee emobj = new task11employee();
+            FileStream fs = new FileStream("employee.txt", FileMode.OpenOrCreate, FileAccess.Write);
+            BinaryFormatter f = new BinaryFormatter();
+            f.Serialize(fs, emobj);
+            fs.Close();
+        }
+
+        public void deserialdata()
+        {
+            FileStream fs = new FileStream("employee.txt", FileMode.Open, FileAccess.Read);
+            BinaryFormatter f = new BinaryFormatter();
+            task11employee emp = (task11employee)f.Deserialize(fs);
+            Console.WriteLine(emp.id);
+            Console.WriteLine(emp.name);
+            Console.WriteLine(emp.gender);
+            Console.WriteLine(emp.age);
+        }
+        public static void Main()
+        {
+            emp1 obj = new emp1();
+            obj.Seritofile();
+            obj.deserialdata();
+        }
+
+    }
+}
+
