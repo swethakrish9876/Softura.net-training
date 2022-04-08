323550bf81c3a939d8d75d4e821493e405075501
swethakrish9876 <101106198+swethakrish9876@users.noreply.github.com>
2022-04-08 19:55:24 +0530

    Add files via upload

---
+++ day25(apr-8)taskgoat.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lynq
+{
+    class taskgoat
+    {
+       public static int numgoat(int legs,int eyes)
+        {
+            int num = 0;
+            num = (legs - (eyes - 1)) / 2;
+            return num;
+
+        }
+        public static void Main()
+        {
+            int eyes=52,legs=80;
+            int goat = numgoat(legs, eyes);
+            
+            Console.WriteLine("no_of_goat is:"+goat);
+            Console.WriteLine("no_of_duck is:"+ (goat - 2));
+
+        }
+    }
+}
